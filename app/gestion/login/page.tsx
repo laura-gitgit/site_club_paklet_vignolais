0aa228d955130b54641b6f00d4b90a50169fe4a9
laura-gitgit <laura.boudard@gmail.com>
2026-02-09 23:00:59 +0100

    Force dynamic login page

--- app/gestion/login/page.tsx
+++ app/gestion/login/page.tsx
@@ -3,6 +3,8 @@ import LoginForm from "./LoginForm";
 
 
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const fetchCache = "force-no-store";
 
 export default function GestionLoginPage() {
   return (
